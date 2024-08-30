cb2e0a0f9104322c3f48c691d7da10c243e36939
-h <liam.gosney07@gmail.com>
2024-08-31 09:33:51 +1000

    v.3

--- cast/run.py
+++ cast/run.py
@@ -6,3 +6,5 @@ create_virtual_audio_sources()
 run_ffmpeg_processes_in_background()
 
 os.system("pactl load-module module-combine-sink sink_name=Combined_Speaker slaves=Upstairs_Living_room_speaker_RTP_sink,Liams_Office_speaker_RTP_sink,Liams_Bedroom_speaker_RTP_sink")
+
+
