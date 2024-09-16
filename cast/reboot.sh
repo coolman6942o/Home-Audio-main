c452d35adc28388f790e041e952ceb637233d1e4
-h <liam.gosney07@gmail.com>
2024-09-17 09:12:22 +1000

    update

--- cast/reboot.sh
+++ cast/reboot.sh
@@ -5,6 +5,5 @@ systemctl --user restart pipewire
 systemctl --user restart pipewire-pulse
 
 pkill ffplay
-pkill spotify
 /home/liam/Downloads/Home-Audio-main/venv/bin/python /home/liam/Downloads/Home-Audio-main/cast/run.py
 spotify
